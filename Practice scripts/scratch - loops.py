822ed53f8e87e12dd0a6a8d29fe1e05b2894e81c
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-12-12 16:38:35 +0100

    Added practice/scratch files for loops & functions

---
+++ Practice scripts/scratch - loops.py
@@ -0,0 +1,27 @@
+## LOOPS ##
+
+# for loops
+var_print = True
+dict_1 = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
+
+if var_print == True:
+    for i in dict_1.keys():
+        print(dict_1[i])
+else:
+    print("Not today!")
+
+for j in dict_1.values():
+    print(j)
+
+for k in dict_1:
+    print(str(k) + " : " + dict_1[k])
+
+for x, y in dict_1.items():
+    print(x, y)
+
+# while loops
+var_w = 1
+
+while var_w <= 9:
+    print(var_w)
+    var_w += 2
