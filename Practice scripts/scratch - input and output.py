6aa75d6c8c3455111e0a49258cdefb45f49b543f
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2025-01-14 16:51:56 +0100

    Added practice file for input/output

---
+++ Practice scripts/scratch - input and output.py
@@ -0,0 +1,11 @@
+# Input
+name = input("Type your name here: ")
+# Output
+print("Hello, ", name)
+
+age = int(input("Type your age as an integer here: "))
+print("You are ", age, " years old!")
+
+height = float(
+    input("Type your height in metres as a floating point number here: "))
+print("Your height is ", height, " meters.")
