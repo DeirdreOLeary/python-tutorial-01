685bba97b0fd6af3665bd83a562d3f344d171a82
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2025-01-14 17:09:09 +0100

    Updated practice file for input/output

--- Practice scripts/scratch - input and output.py
+++ Practice scripts/scratch - input and output.py
@@ -1,11 +1,24 @@
 # Input
 name = input("Type your name here: ")
-# Output
-print("Hello, ", name)
-
 age = int(input("Type your age as an integer here: "))
-print("You are ", age, " years old!")
-
 height = float(
     input("Type your height in metres as a floating point number here: "))
-print("Your height is ", height, " meters.")
+
+# Output
+print("Hello, ", name)
+print("You are ", age, " years old!")
+print("Your height is ", height, " metres.")
+
+# Splitting multiple inputs
+
+# Strings
+firstname, surname = input("First name & surname, please: ").split()
+print("Firstname: ", firstname, "\nSurname: ", surname)
+
+# Integers (works the same for floats)
+x, y, z = map(int, input("Any three numbers: ").split())
+print("Number 1: ", x, "\nNumber 2: ", y, "\nNumber 3: ", z)
+
+# List
+n = list(map(float, input("Type a list of floating point numbers: ").split()))
+print(n)
