d7dd563fdf9a916c4d8d5e116f967d74a06fbc88
Deirdre OLeary <deirdre.e.oleary@gmail.com>
2025-04-17 13:57:01 +0200

    Updated 100 Days of Python challenges: day 38

--- 100 Days/Day38.py
+++ 100 Days/Day38.py
@@ -5,4 +5,14 @@ class CustomException(Exception):
     pass
 
 
-raise CustomException('Testing my custom exception!')
+try:
+    x = int(input("Enter a number: "))
+    y = 100
+    
+    if x == 0:
+        raise CustomException
+    else:
+        print(int(y / x))
+
+except CustomException:
+    print("Invalid number resulting in divide by 0 error")
